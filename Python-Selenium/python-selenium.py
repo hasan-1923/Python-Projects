670d8b494ec4b210a8182fd2e6c0e467d63030b4
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-03-13 23:14:48 +0300

    Add files via upload

--- Python-Selenium/python-selenium.py
+++ Python-Selenium/python-selenium.py
@@ -4,7 +4,7 @@ import time
 
 service=Service("chromedriver.exe")
 driver =webdriver.Chrome(service=service)
-driver.get("http://apple.com")
+driver.get("http://apple.com") # apple.com açılması
 sayfa=driver.current_url
 
 if "apple.com" in  sayfa: 
@@ -13,16 +13,19 @@ if "apple.com" in  sayfa:
 baslik=driver.title
 print("sayfa başligi:" +baslik)
 time.sleep(3)
-driver.maximize_window()
-driver.get("https://www.instagram.com")
+driver.maximize_window()  #program pencereyi büyütür
+driver.get("https://www.instagram.com") #instagram.com açılması
+driver.save_screenshot("./instagram.png")
 sayfa=driver.current_url
 baslik=driver.title
 if "instagram.com" in sayfa:
     print("şuanki sayfa: "+sayfa)
 print("sayfa basligi:"+baslik)
-time.sleep(3)
+time.sleep(3) # program 3 saniye uyur
 driver.back()
 baslik=driver.title
-print("şuanki sayfa basligi:"+baslik)
+driver.save_screenshot("./apple.png") # ekran görüntüsü alır
+if "Apple" in baslik:    
+    print("Apple.com'a geri dönüldü")
 time.sleep(10000)
 
