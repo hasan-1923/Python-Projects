3b987e00cb1417e8a2e8c1b305d55893f706f40b
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-03-13 22:22:05 +0300

    Add files via upload

--- Python-Selenium/python-selenium.py
+++ Python-Selenium/python-selenium.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import datetime
 import time
 
 service=Service("chromedriver.exe")
 driver =webdriver.Chrome(service=service)
 driver.get("http://apple.com")
-
+sayfa=driver.current_url
+print("şuanki sayfa:"+sayfa)
+time.sleep(3)
+driver.get("https://www.instagram.com")
+sayfa=driver.current_url
+print("şuanki sayfa: "+sayfa)
 time.sleep(10000)
 
