3d1b9d0b69ff6a32b556dedac5d8b1053bbdfaa7
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-03-13 22:46:51 +0300

    Add files via upload

--- Python-Selenium/python-selenium.py
+++ Python-Selenium/python-selenium.py
@@ -6,10 +6,23 @@ service=Service("chromedriver.exe")
 driver =webdriver.Chrome(service=service)
 driver.get("http://apple.com")
 sayfa=driver.current_url
-print("şuanki sayfa:"+sayfa)
+
+if "apple.com" in  sayfa: 
+    print("şuanki sayfa:"+ sayfa)
+
+baslik=driver.title
+print("sayfa başligi:" +baslik)
 time.sleep(3)
+driver.maximize_window()
 driver.get("https://www.instagram.com")
 sayfa=driver.current_url
-print("şuanki sayfa: "+sayfa)
+baslik=driver.title
+if "instagram.com" in sayfa:
+    print("şuanki sayfa: "+sayfa)
+print("sayfa basligi:"+baslik)
+time.sleep(3)
+driver.back()
+baslik=driver.title
+print("şuanki sayfa basligi:"+baslik)
 time.sleep(10000)
 
