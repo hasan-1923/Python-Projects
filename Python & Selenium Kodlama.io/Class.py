bff51ce5c0093724e3b1858d3cf9b0172675cdca
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-03-14 15:02:12 +0300

    Add files via upload

--- Python & Selenium Kodlama.io/Class.py
+++ Python & Selenium Kodlama.io/Class.py
@@ -4,17 +4,31 @@ class Student():
         self.name=name
         self.age=age
         self.gender=gender
-NewStudent=Student("Kaan","18","Male")
+NewStudent1=Student("Kaan","18","Male")
 NewStudent2=Student("Çağlar","20","Male")
 NewStudent3=Student("Çağla","24","Female")
-NewStudentA=input("name")
 #Kullanıcıdan veri alıp class eklenmesi
+NewStudentA=input("name:")
 NewStudentB=input("age")
 NewStudentC=input("gender")
 NewStudent4=Student(NewStudentA,NewStudentB,NewStudentC)
 
 #ekrana öğrencilerin bilgilerinin yazdırılması
-print(f'     {NewStudent.name}--   {NewStudent.age} --  {NewStudent.gender}')
-print(f'     {NewStudent2.name} --  {NewStudent2.age}  --{NewStudent2.gender}')
+print(f'     {NewStudent1.name}--   {NewStudent1.age} --  {NewStudent1.gender}')
+print(f'     {NewStudent2.name} --  {NewStudent2.age}  -- {NewStudent2.gender}')
 print(f'     {NewStudent3.name}  -- {NewStudent3.age} --  {NewStudent3.gender}')
 print(f'     {NewStudent4.name}  -- {NewStudent4.age} --  {NewStudent4.gender}')
+
+print("***************************************************************************")
+#öğrencinin bilgilerini değiştirilmesi
+NewStudent1.age=24
+NewStudent2.age=34
+NewStudent3.age=25
+print(f'     {NewStudent1.name}--   {NewStudent1.age} --  {NewStudent1.gender}')
+print(f'     {NewStudent2.name} --  {NewStudent2.age}  -- {NewStudent2.gender}')
+print(f'     {NewStudent3.name}  -- {NewStudent3.age} --  {NewStudent3.gender}')
+print(f'     {NewStudent4.name}  -- {NewStudent4.age} --  {NewStudent4.gender}')
+
+
+
+
