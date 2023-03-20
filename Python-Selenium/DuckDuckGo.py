25d6dd1b9ed381fe30dd896c80436e35e711368d
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-03-20 21:42:28 +0300

    Add files via upload

--- Python-Selenium/DuckDuckGo.py
+++ Python-Selenium/DuckDuckGo.py
@@ -8,12 +8,16 @@ service=Service("chromedriver.exe")
 driver =webdriver.Chrome(service=service)
 driver.get("https://duckduckgo.com/")
 time.sleep(3)
-searchForm=driver.find_element(By.ID,"search_form_input_homepage")
-searchForm.send_keys("selenium")
+searchForm=driver.find_element(By.ID,"search_form_input_homepage") #searchFrom bulma
+searchForm.send_keys("selenium") # searchFrom'a selenium yazdırma 
 time.sleep(2)
-searchButton=driver.find_element(By.ID,"search_button_homepage")
-#searchButton.click() 
-searchButton.send_keys(Keys.ENTER) 
+searchButton=driver.find_element(By.ID,"search_button_homepage")  #searchButton bulma
+#searchButton.click()  
+searchButton.send_keys(Keys.ENTER)  #tıklama
+driver.find_element(By.CLASS_NAME,"Wo6ZAEmESLNUuWBkbMxx").click() #selenium.dev web sitesine giriş yapma
+
+
+
 
 
 
