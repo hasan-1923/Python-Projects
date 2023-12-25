2232684b6bdc1b7bacb9804ff092605bca8f979b
Hasan ÖNEY <116032319+hasan-1923@users.noreply.github.com>
2023-12-25 19:50:26 +0300

    Add files via upload

--- Python-Selenium/TeslaWebSite/Test/test_HomePage.py
+++ Python-Selenium/TeslaWebSite/Test/test_HomePage.py
@@ -1,5 +1,6 @@
 #Tesla WebSite Open and Model Pages
 import pytest
+from Pages.test_cyberTruck import TestCyberTruck
 from Pages.test_testmodell3 import TestTestmodell3
 from Pages.test_testmodels import TestTestmodels
 from Pages.test_testmodelx import TestTestmodelx
@@ -10,14 +11,18 @@ class TestModelClick:
     def test_Car_Model_Click(self):
         test_testmodels=TestTestmodels(self.driver)   # Model-S Page
         test_testmodels.test_testmodelS()
+
         test_testmodell3=TestTestmodell3(self.driver) # Model-3 Page 
         test_testmodell3.test_testmodell33()
+        
         test_testmodelx=TestTestmodelx(self.driver)   # Model-X Page
         test_testmodelx.test_testmodelX()
         
         test_testmodely=TestTestmodely(self.driver)   # Model-Y Page
         test_testmodely.test_testmodelY()
-        
+
+        test_cyberTruck=TestCyberTruck(self.driver)   # Model CyberTruck Page
+        test_cyberTruck.test_cyberTruck()
 
 
         
